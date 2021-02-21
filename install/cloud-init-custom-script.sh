22eb422eedd04ee7a93876485409b4be0178d646
Pavel Jirasek <pavel.jirasek.cz@gmail.com>
2021-02-21 15:38:38 +0100

    Fix: repo bio-class-deb10

--- install/cloud-init-custom-script.sh
+++ install/cloud-init-custom-script.sh
@@ -90,6 +90,7 @@ UserKnownHostsFile=/dev/null" > ~/.ssh/config
 
 else
   # Clone public repo
+  cd /home/debian/;
   git clone https://github.com/bio-platform/bio-class-deb10.git bio-class 2>&1 > /home/debian/gitclone.txt
 
 fi
