2d65e097b77900814c74bb332e405eb94562dac5
Pavel Jirasek <pavel.jirasek.cz@gmail.com>
2021-02-26 07:39:50 +0100

    Deb 10

---
+++ install/install_software_patch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Install patch if difference betweenprepared image/upstream repository versions
+PATH=$PATH;PATH+=":/bin" ;PATH+=":/usr/bin";PATH+=":/usr/sbin";PATH+=":/usr/local/bin"; 
+dirname=$(dirname $0)
+cd "$dirname"
+SCRIPTDIR=$(pwd)
+dirname=$(dirname pwd)
+PATH+=":$dirname"
+export PATH
+
+CONF_DIR="$dirname"/../conf
+LIB_DIR="$dirname"/../lib
+
+BIOUSER=$(curl -s  http://169.254.169.254/openstack/2016-06-30/meta_data.json 2>/dev/null | python -m json.tool | egrep -i Bioclass_user |cut -f 2 -d ':' | tr -d ' ' | sed -rn "s/.*\"(.*)\".*/\1/p"| tr '[:upper:]' '[:lower:]')
+if [[ -z "$BIOUSER" ]]; then
+  echo "Empty Bioclass_user from METADATA, exiting!"
+  exit 1
+fi
+
+echo "Install patch if needed"
+
+# Patch
+
+
+
+# Patch
+echo "Install patch has finished"
+
+# Print user to check in log
+echo "BIOUSER: $BIOUSER"
+
+exit 0
