8b53816503f9cba36dff0fc6d6ac42b78eaaa126
Pavel Jirasek <pavel.jirasek.cz@gmail.com>
2021-02-28 11:26:12 +0100

    Fail2ban

--- install/install_software_patch.sh
+++ install/install_software_patch.sh
@@ -37,6 +37,15 @@ if [[ -f ${CONF_DIR}/nginx-rstudio.conf ]] && [[ -f ${CONF_DIR}/jail.local ]];th
   service fail2ban restart
 fi
 
+# Ignoreip for fail2ban
+BIOSW_IPV4=$(curl -s  http://169.254.169.254/openstack/2016-06-30/meta_data.json 2>/dev/null | python -m json.tool | egrep -i Bioclass_ipv4 |cut -f 2 -d ':' | tr -d ' ' | sed -rn "s/.*\"(.*)\".*/\1/p"| tr '[:upper:]' '[:lower:]' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
+if [[ -n "$BIOSW_IPV4" ]] && [[ -f /etc/fail2ban/jail.local ]];then
+  echo "Inserting $BIOSW_IPV4 into Fail2ban ignoreip"
+  sed -i '/ignoreip/s/$/ '"$BIOSW_IPV4"'/' /etc/fail2ban/jail.local
+  service fail2ban restart
+fi
+
+
 # Patch
 echo "Install patch has finished"
 
